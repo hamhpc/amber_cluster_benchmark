f18d03073200ad2e25174f051f9a4092038f19db
Steve Young <slyoung@hamilton.edu>
2017-01-05 17:33:33 -0500

    Update run_tests.sh
    switch nproc and ns_per_day in results.csv

--- run_tests.sh
+++ run_tests.sh
@@ -70,7 +70,7 @@ echo "NPROC,NS_PER_DAY" > results.csv
     echo "mpiexec -np $NPROC $INTERFACE pmemd.MPI -O -i ~/amber_cluster_benchmark/etc/amber.in -o amber-$NPROC.out -p ~/amber_cluster_benchmark/etc/2e98-hid43-init-ions-wat.prmtop -c ~/amber_cluster_benchmark/etc/amber.rst -r amber-$NPROC.rst -x amber-$NPROC.mdcrd" >> proc-$NPROC/job-$NPROC.run
     echo "NS_PER_DAY=\`cat mdinfo | grep ns/day | tail -1 | awk '{print \$4}'\`" >> proc-$NPROC/job-$NPROC.run
     echo "NPROC=\`pwd | awk -F/ '{print \$6}'|awk -F- '{print \$2}'\`" >> proc-$NPROC/job-$NPROC.run
-    echo 'echo "$NPROC,$NS_PER_DAY" >> ../results.csv' >> proc-$NPROC/job-$NPROC.run
+    echo 'echo "$NS_PER_DAY,$NPROC" >> ../results.csv' >> proc-$NPROC/job-$NPROC.run
     echo "date" >> proc-$NPROC/job-$NPROC.run
     #
     # now submit the job
