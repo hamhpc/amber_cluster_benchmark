807237c350dc85b4fa236917311c9cb555169388
Steve Young <slyoung@hamilton.edu>
2017-01-05 11:10:17 -0500

    Update create_results.sh

--- create_results.sh
+++ create_results.sh
@@ -2,9 +2,13 @@
     #
     # create results file
     #
-    for MDINFO in `ls -1 mdinfo.*`
-    do
-      NPROC=`echo $MDINFO|awk -F. '{print$2}'`
-      NS_PER_DAY=`cat $MDINFO | grep ns/day | tail -1 | awk '{print $4}'`
-      echo "$NPROC,$NS_PER_DAY" >> results.csv
-    done
+    if [ -f mdinfo.* ]; then 
+      for MDINFO in `ls -1 mdinfo.*`
+      do
+        NPROC=`echo $MDINFO|awk -F. '{print$2}'`
+        NS_PER_DAY=`cat $MDINFO | grep ns/day | tail -1 | awk '{print $4}'`
+        echo "$NPROC,$NS_PER_DAY" >> results.csv
+      done
+    else
+       echo "No mdinfo files found!"
+    fi
