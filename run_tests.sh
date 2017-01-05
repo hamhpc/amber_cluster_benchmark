05a0815d0a7a284ea0719b34f244cc6312aae99f
Steve Young <slyoung@hamilton.edu>
2017-01-05 11:02:15 -0500

    Update run_tests.sh

--- run_tests.sh
+++ run_tests.sh
@@ -25,9 +25,9 @@ MPI_MODULE="mpi/mvpaich2-x86_64"
 
 # Send mail when jobs start/stop and abort if enabled
 if [ $SEND_JOB_EMAIL = "Y" ]; then 
-  PBS_QSUB_CMD="qsub -l walltime=1:00:00 -q $QUEUE_NAME $JOB_EMAIL"
+  PBS_QSUB_CMD="qsub -j oe -l walltime=1:00:00 -q $QUEUE_NAME $JOB_EMAIL"
 else
-  PBS_QSUB_CMD="qsub -l walltime=1:00:00 -q $QUEUE_NAME"
+  PBS_QSUB_CMD="qsub -j oe -l walltime=1:00:00 -q $QUEUE_NAME"
 fi
 # turn on verbose MPI logging
 if [ $DEBUGGING = "Y" ]; then 
@@ -37,25 +37,24 @@ fi
 module load $MPI_MODULE
 module load apps/amber14 
   
-for NODES in {1 1 1 1 2 4 8 16 24 32}
-do
-  for NPROC in {1 2 4 8 16 32 64 128 192 256}
+  for NPROC in 1 2 4 8 16 32 64 128 192 256
   do
+    NODES=$(($NPROC/$PROCS_PER_NODE))
+    if [ "$NODES" -le "1" ];  then 
+        NODES=1
+	      PPN=$NPROC
+    else 
+	      PPN=$PROCS_PER_NODE
+    fi
     # 
     # Run the example code for the amount of processors
     #
-    $PBS_QSUB_CMD -N $NPROC -l nodes=$NODES:ppn=$NPROC \
-    mpiexec -np $NPROC $INTERFACE \
-    -env module load $MPI_MODULE \
-    -env module load apps/amber14 \
-    pmemd.MPI -O -i etc/amber.in -o amber-$NPROC.out -p etc/2e98-hid43-init-ions-wat.prmtop -c etc/amber.rst -r amber-$NPROC.rst -x amber-$NPROC.mdcrd
-    #
-    # clean up for next run
-    #
-    mv mdinfo mdinfo.$NPROC
-    mv logfile logfile.$NPROC
-    NS_PER_DAY=`cat mdinfo.$NPROC | grep ns/day | tail -1 | awk '{print $4}'`
-    echo "$NPROC,$NS_PER_DAY" >> results.csv
+    echo 'cd $PBS_O_WORKDIR' > job-$NPROC.run
+    echo 'export PATH=$PBS_O_PATH' >> job-$NPROC.run
+    echo "/usr/lib64/mvapich2/bin/mpiexec -np $NPROC $INTERFACE /usr/local/amber14/bin/pmemd.MPI -O -i etc/amber.in -o amber-$NPROC.out -p etc/2e98-hid43-init-ions-wat.prmtop -c etc/amber.rst -r amber-$NPROC.rst -x amber-$NPROC.mdcrd" >> job-$NPROC.run
+    echo "mv mdinfo mdinfo.$NPROC" >> job-$NPROC.run
+    echo "mv logfile logfile.$NPROC" >> job-$NPROC.run
+    $PBS_QSUB_CMD -N $NPROC -l nodes=$NODES:ppn=$PPN job-$NPROC.run
     
   done   
 done
