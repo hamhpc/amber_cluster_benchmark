1e0d04f2b9b5fc8939105da7897df4c2ac92a649
Steve Young <slyoung@hamilton.edu>
2017-04-24 11:00:24 -0400

    Update run_tests.sh

--- run_tests.sh
+++ run_tests.sh
@@ -4,6 +4,16 @@
 #
 
 #
+# Amber VARIABLES
+#
+APPLICATION=pmemd.MPI
+AMBER_IN=~/amber_cluster_benchmark/etc/amber.in
+AMBER_OUT=amber-$NPROC.out
+PRMTOP=~/amber_cluster_benchmark/etc/2e98-hid43-init-ions-wat.prmtop
+RESTART_IN=~/amber_cluster_benchmark/etc/amber.rst 
+RESTART_OUT=~/amber_cluster_benchmark/etc/amber-$NPROC.rst 
+COORD=amber-$NPROC.mdcrd
+#
 # Declare some variables
 #
 QUEUE_NAME="una"
@@ -82,7 +92,7 @@ echo "NS_PER_DAY,NPROC" > $RESULTS_FILE.csv
     echo 'export PATH=$PBS_O_PATH' >> proc-$NPROC/job-$NPROC.run
     echo "source ../load_modules" >> proc-$NPROC/job-$NPROC.run
     echo "date" >> proc-$NPROC/job-$NPROC.run
-    echo "/usr/bin/time mpiexec -np $NPROC $INTERFACE pmemd.MPI -O -i ~/amber_cluster_benchmark/etc/amber.in -o amber-$NPROC.out -p ~/amber_cluster_benchmark/etc/2e98-hid43-init-ions-wat.prmtop -c ~/amber_cluster_benchmark/etc/amber.rst -r amber-$NPROC.rst -x amber-$NPROC.mdcrd" >> proc-$NPROC/job-$NPROC.run
+    echo "/usr/bin/time mpiexec -np $NPROC $INTERFACE $APPLICATION -O -i $AMBER_IN -o $AMBER_OUT -p $PRMTOP -c $RESTART_IN -r $RESTART_OUT -x $COORD" >> proc-$NPROC/job-$NPROC.run
     echo "NS_PER_DAY=\`cat mdinfo | grep ns/day | tail -1 | awk '{print \$4}'\`" >> proc-$NPROC/job-$NPROC.run
     echo "NPROC=\`pwd | awk -F/ '{print \$6}'|awk -F- '{print \$2}'\`" >> proc-$NPROC/job-$NPROC.run
     echo 'echo "$NS_PER_DAY,$NPROC" >> ../RESULTS_FILE.csv' >> proc-$NPROC/job-$NPROC.run
